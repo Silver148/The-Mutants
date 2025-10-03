6fca1f90ccb363b64f09612964e6847798cb0f94
silverhacker <juanjoseyaguaro65@gmail.com>
2025-10-03 15:55:05 -0400

    Create Window SDL2

---
+++ Makefile
@@ -0,0 +1,18 @@
+NAME = The_Mutants
+CC = gcc
+CFLAGS = -Wall -Iinclude
+LIBS = -lSDL2
+SOURCES = src/main.c
+OBJECTS = $(SOURCES:src/%.c=obj/%.o)
+
+$(NAME): $(OBJECTS)
+	$(CC) $(CFLAGS) $(OBJECTS) -o $(NAME) $(LIBS)
+
+obj/%.o: src/%.c
+	mkdir -p obj
+	$(CC) $(CFLAGS) -c $< -o $@
+
+all: $(NAME)
+
+clean:
+	rm -f obj $(NAME)
--- src/main.c
+++ src/main.c
@@ -4,9 +4,34 @@ File: main.c
 */
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 //Main
 int main()
-{
+{   
+    SDL_Window* window = NULL;
 
+    /*Initialize SDL*/
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /*Create window*/
+    window = SDL_CreateWindow("The Mutants", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if(window == NULL)
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+
+    while(1)
+    {
+        SDL_Event e;
+        if(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT)
+                break;
+        }
+    }
+
+    return 0;
 }
