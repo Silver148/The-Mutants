954c0af89182b39161bf05e8c98742c14ac7490c
silverhacker <juanjoseyaguaro65@gmail.com>
2025-10-03 16:08:44 -0400

    CONFIDENTIAL

--- src/main.c
+++ src/main.c
@@ -1,25 +1,29 @@
 /*
 Authors: Abel Ferrer(aka The_Light), Juan Yaguaro(aka silverhacker), Sebastián Colina(aka ?)
 File: main.c
+
+THIS CODE WILL REMAIN CONFIDENTIAL UNTIL THE PROJECT IS COMPLETED. 
+Anyone who leaks this code will be automatically kicked out of the 
+group and will be considered a real gay man.
 */
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-//Main
+//Func Main
 int main()
 {   
     SDL_Window* window = NULL;
 
     /*Initialize SDL*/
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    if(SDL_Init(SDL_INIT_VIDEO) && SDL_Init(SDL_INIT_AUDIO) < 0)
     {
         printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
         return 1;
     }
 
     /*Create window*/
-    window = SDL_CreateWindow("The Mutants", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("The Mutant´s", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
     if(window == NULL)
         printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
 
@@ -28,7 +32,7 @@ int main()
         SDL_Event e;
         if(SDL_PollEvent(&e))
         {
-            if(e.type == SDL_QUIT)
+            if(e.type == SDL_QUIT) /*EXIT*/
                 break;
         }
     }
