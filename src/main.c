9ae1ed8b0ae29bedd0fcc66d0ba526a135101bb1
silverhacker <juanjoseyaguaro65@gmail.com>
2025-10-04 20:29:29 -0400

    COPYRIGHT 2025 FIX

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ THIS CODE WILL REMAIN CONFIDENTIAL UNTIL THE PROJECT IS COMPLETED.
 Anyone who leaks this code will be automatically kicked out of the 
 group and will be considered a real gay.
 
-Copyright 2024
+Copyright 2025
 */
 
 #include <stdio.h>
