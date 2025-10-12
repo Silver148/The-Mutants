74ab03d7d5ed45dae554ba3d4df21f1e5af2172f
silverhacker <juanjoseyaguaro65@gmail.com>
2025-10-12 16:24:43 -0400

    Revertiendo unos commits fallidos :)

--- new_commit.c
+++ new_commit.c
@@ -9,19 +9,7 @@ Copyright 2025
 
 int main()
 {
-    char filesToAdd[256];
-    printf("Ingresa los archivos a agregar (o . para todos):\n");
-    scanf("%s", filesToAdd); //Escanear archivos a agregar
-
-    if(filesToAdd[0] == '\0') //Si no se ingresa nada
-    {
-        printf("No se ingresaron archivos. Saliendo...\n");
-        return 1;
-    }
-
-    char addCommand[512];
-    snprintf(addCommand, sizeof(addCommand), "git add %s", filesToAdd); //Crear comando
-    system(addCommand); //Ejecutar comando
+    system("git add .");
 
     printf("Ingresa nombre del commit:\n");
     char commitMessage[256];
